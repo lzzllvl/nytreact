f1460cffb37cbbd3cc6a34b7e02c62b51504b3c9
Greg Lee <gml2136@gmail.com>
2017-06-02 00:25:16 -0700

    Initial Site files and minor server code

---
+++ server.js
@@ -0,0 +1,36 @@
+//server dependencies and constants
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const Promise = require("bluebird");
+
+
+const PORT = process.env.PORT || 8080;
+
+//models and ODM
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/nytreact");
+
+var db = mongoose.connection;
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
+//router config
+const router = express.Router();
+require('./routes/apiRoutes')(router, db);
+require('./routes/spaRoutes')(router, db);
+
+//server init and middleware config
+const app = express();
+app.use('/', router);
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static("public"));
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}...`));
